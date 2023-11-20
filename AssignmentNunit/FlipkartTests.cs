e0cfcafa7394f89dbc36ffb44d6108be81a9de97
AdithyanPadmanabhan <197160@ust.com>
2023-11-20 18:03:15 +0530

    20-11-2023

--- AssignmentNunit/FlipkartTests.cs
+++ AssignmentNunit/FlipkartTests.cs
@@ -13,7 +13,8 @@ namespace AssignmentNunit
     internal class FlipkartTests : CoreCodes
     {
         [Test]
-        public void SearchProduct()
+        [Order(1)]
+        public void SearchProductTest()
         {
            
 
@@ -28,9 +29,44 @@ namespace AssignmentNunit
             IWebElement searchPrdouct = fluentWait.Until(d => d.FindElement(By.ClassName("Pke_EE")));
             searchPrdouct.SendKeys("laptop");
             searchPrdouct.SendKeys(Keys.Enter);
-            //IWebElement searchButton = fluentWait.Until(d => d.FindElement(By.ClassName("_2iLD__")));
-            //searchButton.Click();
             
+        }
+
+        [Test]
+        [Order(2)]
+        public void AddToCartTest()
+        {
+
+            DefaultWait<IWebDriver> fluentWait = new DefaultWait<IWebDriver>(driver);
+            fluentWait.Timeout = TimeSpan.FromSeconds(5);
+            fluentWait.PollingInterval = TimeSpan.FromMilliseconds(100);
+            fluentWait.IgnoreExceptionTypes(typeof(NoSuchElementException));
+            fluentWait.Message = "Product not found";
+
+            IWebElement lapTopSelection = fluentWait.Until(d => d.FindElement(By.XPath
+                ("//div[@class='_2kHMtA'][1]")));
+            lapTopSelection.Click();
+
+            //List<string> listWindow = driver.WindowHandles.ToList();
+
+            //string lastWindowhandle = "";
+            //foreach (var handle in listWindow)
+            //{
+                
+            //    driver.SwitchTo().Window(handle);
+            //    Thread.Sleep(3000);
+
+            //}
+           Thread.Sleep(3000);
+            IWebElement addCartButton = fluentWait.Until(d => d.FindElement(
+                By.XPath("//button[@class='_2KpZ6l _2U9uOA _3v1-ww']")));
+                   addCartButton.Click();
+            Thread.Sleep(3000);
+
+            IWebElement Cart = fluentWait.Until(d => d.FindElement(
+               By.XPath("//div[@class='YUhWwv']")));
+            Cart.Click();
+
         }
     }
 }
